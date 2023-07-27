f02b1518ec93ec811bfc2752f2c56d8ff3de5dd5
Tiago Gomes <tidgomes@gmail.com>
2023-07-27 13:53:45 -0300

    apply useContext to display the amount of items in the cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
+import { useContext } from 'react'
 import CartIcon from '../Cart/CartIcon'
+import CartContext from '../../store/cart-context'
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = ({ onClick }) => {
+  const ctx = useContext(CartContext)
+
+  const amountOfCartItems = ctx.items.reduce((amount, item) => {
+    return amount + item.amount
+  }, 0)
+
   return (
     <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{amountOfCartItems}</span>
     </button>
   )
 }
