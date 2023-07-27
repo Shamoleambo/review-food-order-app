75712b69ad2dd188660ca22a7ce2fc11da9dea2d
Tiago Gomes <tidgomes@gmail.com>
2023-07-27 16:37:25 -0300

    animate the button when adding an item

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,34 @@
-import { useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import CartIcon from '../Cart/CartIcon'
 import CartContext from '../../store/cart-context'
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = ({ onClick }) => {
+  const [btnIsHighlited, setBtnIsHighlighted] = useState(false)
   const ctx = useContext(CartContext)
+
+  const { items } = ctx
 
   const amountOfCartItems = ctx.items.reduce((amount, item) => {
     return amount + item.amount
   }, 0)
 
+  const btnClasses = `${classes.button} ${btnIsHighlited ? classes.bump : ''}`
+
+  useEffect(() => {
+    if (items.length === 0) return
+
+    setBtnIsHighlighted(true)
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false)
+    }, 300)
+
+    return () => clearTimeout(timer)
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
